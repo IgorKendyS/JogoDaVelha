3219f6f66b8126872d01808543009fafdc9686a9
igorkendyS <igor-kendy@hotmail.com>
2024-12-22 00:10:10 -0300

    Jogo da Velha 1.0

--- JogoDaVelha/JogoDaVelha.Designer.cs
+++ JogoDaVelha/JogoDaVelha.Designer.cs
@@ -38,99 +38,148 @@
             this.btn4 = new System.Windows.Forms.Button();
             this.btn8 = new System.Windows.Forms.Button();
             this.btn9 = new System.Windows.Forms.Button();
+            this.btnRestart = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(204, 21);
+            this.label1.Location = new System.Drawing.Point(153, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(207, 36);
+            this.label1.Size = new System.Drawing.Size(167, 29);
             this.label1.TabIndex = 1;
             this.label1.Text = "Jogo da Velha";
             // 
             // btn3
             // 
-            this.btn3.Location = new System.Drawing.Point(409, 89);
+            this.btn3.BackColor = System.Drawing.Color.Tomato;
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(307, 72);
+            this.btn3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn3.Name = "btn3";
-            this.btn3.Size = new System.Drawing.Size(138, 124);
+            this.btn3.Size = new System.Drawing.Size(104, 101);
             this.btn3.TabIndex = 3;
-            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.btn_Click);
             // 
             // btn2
             // 
-            this.btn2.Location = new System.Drawing.Point(247, 89);
+            this.btn2.BackColor = System.Drawing.Color.Tomato;
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(185, 72);
+            this.btn2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn2.Name = "btn2";
-            this.btn2.Size = new System.Drawing.Size(138, 124);
+            this.btn2.Size = new System.Drawing.Size(104, 101);
             this.btn2.TabIndex = 9;
-            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn_Click);
             // 
             // btn1
             // 
-            this.btn1.Font = new System.Drawing.Font("Onyx", 8.25F);
-            this.btn1.Location = new System.Drawing.Point(77, 89);
+            this.btn1.BackColor = System.Drawing.Color.Tomato;
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(58, 72);
+            this.btn1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn1.Name = "btn1";
-            this.btn1.Size = new System.Drawing.Size(138, 124);
+            this.btn1.Size = new System.Drawing.Size(104, 101);
             this.btn1.TabIndex = 0;
-            this.btn1.UseVisualStyleBackColor = true;
-            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn_Click);
             // 
             // btn5
             // 
-            this.btn5.Location = new System.Drawing.Point(247, 230);
+            this.btn5.BackColor = System.Drawing.Color.Tomato;
+            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(185, 187);
+            this.btn5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn5.Name = "btn5";
-            this.btn5.Size = new System.Drawing.Size(138, 124);
+            this.btn5.Size = new System.Drawing.Size(104, 101);
             this.btn5.TabIndex = 2;
-            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.btn_Click);
             // 
             // btn6
             // 
-            this.btn6.Location = new System.Drawing.Point(409, 230);
+            this.btn6.BackColor = System.Drawing.Color.Tomato;
+            this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(307, 187);
+            this.btn6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn6.Name = "btn6";
-            this.btn6.Size = new System.Drawing.Size(138, 124);
+            this.btn6.Size = new System.Drawing.Size(104, 101);
             this.btn6.TabIndex = 4;
-            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.btn_Click);
             // 
             // btn7
             // 
-            this.btn7.Location = new System.Drawing.Point(77, 380);
+            this.btn7.BackColor = System.Drawing.Color.Tomato;
+            this.btn7.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(58, 309);
+            this.btn7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn7.Name = "btn7";
-            this.btn7.Size = new System.Drawing.Size(138, 127);
+            this.btn7.Size = new System.Drawing.Size(104, 103);
             this.btn7.TabIndex = 5;
-            this.btn7.UseVisualStyleBackColor = true;
-            this.btn7.Click += new System.EventHandler(this.button5_Click);
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.btn_Click);
             // 
             // btn4
             // 
-            this.btn4.Location = new System.Drawing.Point(77, 230);
+            this.btn4.BackColor = System.Drawing.Color.Tomato;
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(58, 187);
+            this.btn4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn4.Name = "btn4";
-            this.btn4.Size = new System.Drawing.Size(138, 124);
+            this.btn4.Size = new System.Drawing.Size(104, 101);
             this.btn4.TabIndex = 6;
-            this.btn4.UseVisualStyleBackColor = true;
-            this.btn4.Click += new System.EventHandler(this.button6_Click);
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.btn_Click);
             // 
             // btn8
             // 
-            this.btn8.Location = new System.Drawing.Point(247, 380);
+            this.btn8.BackColor = System.Drawing.Color.Tomato;
+            this.btn8.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(185, 309);
+            this.btn8.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn8.Name = "btn8";
-            this.btn8.Size = new System.Drawing.Size(138, 124);
+            this.btn8.Size = new System.Drawing.Size(104, 101);
             this.btn8.TabIndex = 7;
-            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.btn_Click);
             // 
             // btn9
             // 
-            this.btn9.Location = new System.Drawing.Point(409, 380);
+            this.btn9.BackColor = System.Drawing.Color.Tomato;
+            this.btn9.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(307, 309);
+            this.btn9.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn9.Name = "btn9";
-            this.btn9.Size = new System.Drawing.Size(138, 124);
+            this.btn9.Size = new System.Drawing.Size(104, 101);
             this.btn9.TabIndex = 8;
-            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnRestart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestart.Location = new System.Drawing.Point(168, 443);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(138, 23);
+            this.btnRestart.TabIndex = 10;
+            this.btnRestart.Text = "REINICIAR JOGO";
+            this.btnRestart.UseVisualStyleBackColor = false;
+            this.btnRestart.Visible = false;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
             // 
             // JogoDaVelha
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(651, 538);
+            this.BackColor = System.Drawing.Color.YellowGreen;
+            this.ClientSize = new System.Drawing.Size(488, 488);
+            this.Controls.Add(this.btnRestart);
             this.Controls.Add(this.btn2);
             this.Controls.Add(this.btn9);
             this.Controls.Add(this.btn8);
@@ -141,9 +190,9 @@
             this.Controls.Add(this.btn5);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btn1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "JogoDaVelha";
             this.Text = "Jogo da Velha";
-            this.Load += new System.EventHandler(this.JogoDaVelha_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -160,6 +209,7 @@
         private System.Windows.Forms.Button btn4;
         private System.Windows.Forms.Button btn8;
         private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btnRestart;
     }
 }
 
--- JogoDaVelha/JogoDaVelha.cs
+++ JogoDaVelha/JogoDaVelha.cs
@@ -1,42 +1,96 @@
-﻿using ReaLTaiizor.Forms;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Media;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows.Forms;
 
 namespace JogoDaVelha
 {
     public partial class JogoDaVelha : Form
     {
+        private bool turnoX = true;
+        private int jogadas = 0;
+
         public JogoDaVelha()
         {
             InitializeComponent();
         }
 
-        private void button6_Click(object sender, EventArgs e)
+        private void btn_Click(object sender, EventArgs e)
         {
+            Button btn = (Button)sender;
 
+            if (btn.Text != "")
+            {
+                MessageBox.Show("Essa célula já foi escolhida!");
+                return;
+            }
+
+            btn.Text = turnoX ? "X" : "O";
+            btn.Enabled = false;
+
+            jogadas++;
+
+            if (VerificarVencedor())
+            {
+                MessageBox.Show($"Jogador {(turnoX ? "X" : "O")} venceu!");
+                btnRestart.Visible = true;
+                return;
+            }
+
+            if (jogadas == 9)
+            {
+                MessageBox.Show("Empate!");
+                btnRestart.Visible = true;
+                return;
+            }
+
+            turnoX = !turnoX;
         }
 
-        private void button5_Click(object sender, EventArgs e)
+        private bool VerificarVencedor()
         {
+            return (
+                (btn1.Text == btn2.Text && btn2.Text == btn3.Text && btn1.Text != "") ||
+                (btn4.Text == btn5.Text && btn5.Text == btn6.Text && btn4.Text != "") ||
+                (btn7.Text == btn8.Text && btn8.Text == btn9.Text && btn7.Text != "") ||
 
+                (btn1.Text == btn4.Text && btn4.Text == btn7.Text && btn1.Text != "") ||
+                (btn2.Text == btn5.Text && btn5.Text == btn8.Text && btn2.Text != "") ||
+                (btn3.Text == btn6.Text && btn6.Text == btn9.Text && btn3.Text != "") ||
+
+                (btn1.Text == btn5.Text && btn5.Text == btn9.Text && btn1.Text != "") ||
+                (btn3.Text == btn5.Text && btn5.Text == btn7.Text && btn3.Text != "")
+            );
         }
 
-        private void btn1_Click(object sender, EventArgs e)
+        private void ReiniciarJogo()
         {
+            btnRestart.Visible = false;
 
+            btn1.Text = "";
+            btn1.Enabled = true;
+            btn2.Text = "";
+            btn2.Enabled = true;
+            btn3.Text = "";
+            btn3.Enabled = true;
+            btn4.Text = "";
+            btn4.Enabled = true;
+            btn5.Text = "";
+            btn5.Enabled = true;
+            btn6.Text = "";
+            btn6.Enabled = true;
+            btn7.Text = "";
+            btn7.Enabled = true;
+            btn8.Text = "";
+            btn8.Enabled = true;
+            btn9.Text = "";
+            btn9.Enabled = true;
+
+            turnoX = true;
+            jogadas = 0;
         }
 
-        public void JogoDaVelha_Load(object sender, EventArgs e)
+        private void btnRestart_Click(object sender, EventArgs e)
         {
-            bool memory = true;
+            ReiniciarJogo();
         }
     }
 }
